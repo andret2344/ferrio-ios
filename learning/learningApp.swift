c0304eff2487647baf0d3eaae5f1ed69984e794e
andret2344 <andret2344@gmail.com>
2023-08-28 17:54:12 +0200

    Initial Commit

---
+++ learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by Andrzej Chmiel on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
