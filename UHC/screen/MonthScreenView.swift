0513e33bc7c3acea1ef1388ecfbb825ad0933dff
andret2344 <andret2344@gmail.com>
2023-09-10 21:53:49 +0200

    Added randomize option
    Took 54 minutes

--- UHC/screen/MonthScreenView.swift
+++ UHC/screen/MonthScreenView.swift
@@ -8,13 +8,15 @@ struct MonthScreenView: View {
 	let holidayDays: [HolidayDay]
 	@State
 	private var selection = Calendar.current.component(.month, from: Date()) - 1
+	@State
+	private var selectedDay: HolidayDay? = nil
 	var body: some View {
 		LazyHStack {
 			NavigationStack {
 				TabView(selection: $selection) {
 					ForEach(1..<13) { i in
 						ZStack {
-							MonthAdapter(month: i, days: holidayDays)
+							MonthAdapter(selectedDay: $selectedDay, month: i, days: holidayDays)
 						}
 					}
 				}
@@ -30,10 +32,22 @@ struct MonthScreenView: View {
 											.accessibilityLabel("Today")
 								}
 							}
+							ToolbarItem(placement: .primaryAction) {
+								Button {
+									selectedDay = getRandomHolidayDay()
+								} label: {
+									Image(systemName: "shuffle")
+											.accessibilityLabel("Random")
+								}
+							}
 						}
 			}
 					.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
 					.tabViewStyle(.page(indexDisplayMode: .never))
 		}
+	}
+
+	func getRandomHolidayDay() -> HolidayDay {
+		holidayDays[Int.random(in: 0..<holidayDays.count)];
 	}
 }
