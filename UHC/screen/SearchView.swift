ac634df92e8f5f9f158651b5c63c21fe73802a1b
andret2344 <andret2344@gmail.com>
2023-09-20 00:22:26 +0200

    Fixed the sheet
    Took 26 minutes

---
+++ UHC/screen/SearchView.swift
@@ -0,0 +1,37 @@
+//
+// Created by Andrzej Chmiel on 20/09/2023.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+	let searchText: String
+	let holidayDays: [HolidayDay]
+	@Binding
+	var selectedDay: HolidayDay?
+	var body: some View {
+		if searchText != "" {
+			List {
+				ForEach(holidayDays, id: \.id) { holidayDay in
+					let holidays = holidayDay.holidays.filter { holiday in
+						holiday.name.localizedCaseInsensitiveContains(searchText)
+					}
+					if holidays.count > 0 {
+						HStack {
+							Text("\(holidayDay.getDate()):")
+							Divider()
+							VStack(alignment: .leading) {
+								ForEach(holidays, id: \.id) { holiday in
+									Text("- \(holiday.name)")
+								}
+							}
+						}
+								.onTapGesture {
+									selectedDay = holidayDay
+								}
+					}
+				}
+			}
+		}
+	}
+}
