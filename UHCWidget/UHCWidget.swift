08f4a7e3b987f421344f59cd1c6d3f26d7c84b3a
andret2344 <andret2344@gmail.com>
2023-09-21 23:00:02 +0200

    Fixed widget and a bit translations
    Took 3 hours 10 minutes

--- UHCWidget/UHCWidget.swift
+++ UHCWidget/UHCWidget.swift
@@ -6,29 +6,33 @@ import WidgetKit
 import SwiftUI
 import Intents
 
-struct Provider: TimelineProvider {
+struct Provider: IntentTimelineProvider {
+	typealias Entry = WidgetEntry
+	typealias Intent = ConfigurationIntent
+
 	func placeholder(in context: Context) -> WidgetEntry {
 		let holiday: Holiday = Holiday(id: 1, usual: true, name: "Perfect day!", description: "", url: "")
-		return WidgetEntry(date: Date(), holidayDay: HolidayDay(id: "-1", day: 1, month: 1, holidays: [holiday]))
+		return WidgetEntry(date: Date(), holidayDay: HolidayDay(id: "-1", day: 1, month: 1, holidays: [holiday]), dayOffset: 0, colorized: false)
 	}
 
-	func getSnapshot(in context: Context, completion: @escaping (WidgetEntry) -> ()) -> Void {
+	func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (WidgetEntry) -> Void) {
 		Task {
-			guard let holidayDay = try? await URLSession.shared.decode(HolidayDay.self, from: getUrl()) else {
+			guard let holidayDay = try? await URLSession.shared.decode(HolidayDay.self, from: getUrl(plusDays: 0)) else {
 				return
 			}
-			let entry = WidgetEntry(date: Date(), holidayDay: holidayDay)
+			let entry = WidgetEntry(date: Date(), holidayDay: holidayDay, dayOffset: 0, colorized: false)
 			completion(entry)
 		}
 	}
 
-	func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) -> Void {
+	func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<WidgetEntry>) -> Void) {
 		Task {
-			guard let holidayDay = try? await URLSession.shared.decode(HolidayDay.self, from: getUrl()) else {
+			let plusDays = configuration.plusDays?.intValue ?? 0
+			guard let holidayDay = try? await URLSession.shared.decode(HolidayDay.self, from: getUrl(plusDays: plusDays)) else {
 				return
 			}
 			let target: Date = Calendar.current.date(bySettingHour: 0, minute: .random(in: 0..<3), second: .random(in: 0..<59), of: Date())!
-			let entry: WidgetEntry = WidgetEntry(date: getProperDate(targetDate: target), holidayDay: holidayDay)
+			let entry: WidgetEntry = WidgetEntry(date: getProperDate(targetDate: target), holidayDay: holidayDay, dayOffset: plusDays, colorized: configuration.colorized?.boolValue ?? false)
 			completion(Timeline(entries: [entry], policy: .after(target)))
 		}
 	}
@@ -41,8 +45,9 @@ struct Provider: TimelineProvider {
 		return Calendar.current.date(byAdding: .day, value: 1, to: targetDate)!
 	}
 
-	func getUrl() -> URL {
-		let components: DateComponents = Calendar.current.dateComponents([.day, .month], from: Date())
+	func getUrl(plusDays: Int) -> URL {
+		let current: Date = Calendar.current.date(byAdding: .day, value: plusDays, to: Date())!
+		let components: DateComponents = Calendar.current.dateComponents([.day, .month], from: current)
 		let code: String? = Locale.current.language.languageCode?.identifier
 		let lang: String = ["pl"].contains(code!) ? code! : "en"
 		return URL(string: "https://api.unusualcalendar.net/holiday/\(lang)/day/\(components.month!)/\(components.day!)")!
@@ -52,6 +57,8 @@ struct Provider: TimelineProvider {
 struct WidgetEntry: TimelineEntry {
 	let date: Date
 	let holidayDay: HolidayDay
+	let dayOffset: Int
+	let colorized: Bool
 }
 
 struct UHCWidgetEntryView: View {
@@ -61,25 +68,34 @@ struct UHCWidgetEntryView: View {
 					"- \(holiday.name)"
 				}
 				.joined(separator: "\n");
-		HStack {
+		VStack {
+			Text("\(entry.holidayDay.getDate()):")
+					.bold()
+					.padding(10)
+					.frame(maxWidth: .infinity)
 			if text == "" {
-				VStack {
-					Text("No unusual holidays today.")
-							.font(.body)
-							.multilineTextAlignment(.center)
-					Image("SadIcon")
-				}
+				Text("No unusual holidays today.")
+						.font(.body)
+						.multilineTextAlignment(.center)
+				Image("SadIcon")
 			} else {
-				Text(text).font(.caption)
+				Text(text)
+						.font(.caption)
+						.padding(5)
+						.frame(maxWidth: .infinity, alignment: .topLeading)
 			}
 		}
+				.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+				.background(entry.colorized ? Color(UIColor.random(seed: Int(entry.holidayDay.id)!)) : nil)
+				.overlay(RoundedRectangle(cornerRadius: 23)
+						.stroke(.black, lineWidth: 4))
 	}
 }
 
 struct UHCWidget: Widget {
 	let kind: String = "UHCWidget"
 	var body: some WidgetConfiguration {
-		StaticConfiguration(kind: kind, provider: Provider()) { entry in
+		IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
 			UHCWidgetEntryView(entry: entry)
 		}
 				.configurationDisplayName("Unusual Holiday Calendar")
