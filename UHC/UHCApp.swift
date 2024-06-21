e9a57b97ffad9e7abcceea725b3c05afe551e66f
andret2344 <andret2344@gmail.com>
2024-06-21 07:47:05 +0200

    #6: WIP: Firebase auth

--- UHC/UHCApp.swift
+++ UHC/UHCApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct UHCApp: App {
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
 	var body: some Scene {
 		WindowGroup {
-			ContentView()
+			
 		}
 	}
 }
