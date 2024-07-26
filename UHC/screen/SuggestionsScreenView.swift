f109bf038a2738af10804117c4e80de8e126eaec
andret2344 <andret2344@gmail.com>
2024-07-26 09:23:33 +0200

    #2: Now both floating and fixed reports are visible

--- UHC/screen/SuggestionsScreenView.swift
+++ UHC/screen/SuggestionsScreenView.swift
@@ -6,37 +6,87 @@ import FirebaseAuth
 import SwiftUI
 
 struct SuggestionsScreenView: View {
-	@State var suggestions: [MissingFixedHoliday] = []
+	@State private var suggestionsFixed: [MissingFixedHoliday] = []
+	@State private var suggestionsFloating: [MissingFloatingHoliday] = []
+	@State private var reportedHolidaysType: String = "fixed"
 
 	var body: some View {
+		Picker("Select reported holidays type", selection: $reportedHolidaysType) {
+			Text("Fixed").tag("fixed")
+			Text("Floating").tag("floating")
+		}
+		.pickerStyle(.segmented)
 		List {
-			ForEach(suggestions, id: \.id) { suggestion in
-				HStack {
-					VStack {
-						Text("\(String(format: "%02d", suggestion.month)).\(String(format: "%02d", suggestion.day))")
-							.italic()
-							.frame(maxWidth: .infinity, alignment: .leading)
-						Text(suggestion.name)
-							.bold()
-							.lineLimit(1)
-							.frame(maxWidth: .infinity, alignment: .leading)
-						Text(suggestion.description)
-							.lineLimit(2)
-							.frame(maxWidth: .infinity, alignment: .leading)
+			if reportedHolidaysType == "fixed" {
+				ForEach(suggestionsFixed, id: \.id) { suggestion in
+					HStack {
+						VStack {
+							Text("\(String(format: "%02d", suggestion.month)).\(String(format: "%02d", suggestion.day))")
+								.italic()
+								.frame(maxWidth: .infinity, alignment: .leading)
+							Text(suggestion.name)
+								.bold()
+								.lineLimit(1)
+								.frame(maxWidth: .infinity, alignment: .leading)
+							Text(suggestion.description)
+								.lineLimit(2)
+								.frame(maxWidth: .infinity, alignment: .leading)
+						}
+						Spacer()
+						Text(suggestion.reportState.rawValue.localized())
+							.foregroundStyle(Color(UIColor.systemBackground))
+							.frame(width: 128, height: 32)
+							.background(RoundedRectangle(cornerRadius: 8).fill(getColor(reportState: suggestion.reportState)))
+							.overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
 					}
-					Spacer()
-					Text(suggestion.reportState.rawValue.localized())
-						.foregroundStyle(Color(UIColor.systemBackground))
-						.frame(width: 128, height: 32)
-						.background(RoundedRectangle(cornerRadius: 8).fill(getColor(reportState: suggestion.reportState)))
-						.overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
+				}
+			} else {
+				ForEach(suggestionsFloating, id: \.id) { suggestion in
+					HStack {
+						VStack {
+							Text(suggestion.date)
+								.italic()
+								.frame(maxWidth: .infinity, alignment: .leading)
+							Text(suggestion.name)
+								.bold()
+								.lineLimit(1)
+								.frame(maxWidth: .infinity, alignment: .leading)
+							Text(suggestion.description)
+								.lineLimit(2)
+								.frame(maxWidth: .infinity, alignment: .leading)
+						}
+						Spacer()
+						Text(suggestion.reportState.rawValue.localized())
+							.foregroundStyle(Color(UIColor.systemBackground))
+							.frame(width: 128, height: 32)
+							.background(RoundedRectangle(cornerRadius: 8).fill(getColor(reportState: suggestion.reportState)))
+							.overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
+					}
 				}
 			}
 		}
 		.navigationTitle("My suggestions")
 		.task {
 			do {
-				suggestions = try await URLSession.shared.decode([MissingFixedHoliday].self, from: getUrl(), keyDecodingStrategy: .convertFromSnakeCase)
+				suggestionsFixed = try await URLSession.shared.decode([MissingFixedHoliday].self, from: getUrlForFixed(), keyDecodingStrategy: .convertFromSnakeCase)
+			} catch let DecodingError.dataCorrupted(context) {
+				print(context)
+			} catch let DecodingError.keyNotFound(key, context) {
+				print("Key '\(key)' not found:", context.debugDescription)
+				print("codingPath:", context.codingPath)
+			} catch let DecodingError.valueNotFound(value, context) {
+				print("Value '\(value)' not found:", context.debugDescription)
+				print("codingPath:", context.codingPath)
+			} catch let DecodingError.typeMismatch(type, context) {
+				print("Type '\(type)' mismatch:", context.debugDescription)
+				print("codingPath:", context.codingPath)
+			} catch {
+				print("error: ", error)
+			}
+		}
+		.task {
+			do {
+				suggestionsFloating = try await URLSession.shared.decode([MissingFloatingHoliday].self, from: getUrlForFloating(), keyDecodingStrategy: .convertFromSnakeCase)
 			} catch let DecodingError.dataCorrupted(context) {
 				print(context)
 			} catch let DecodingError.keyNotFound(key, context) {
@@ -54,9 +104,14 @@ struct SuggestionsScreenView: View {
 		}
 	}
 
-	func getUrl() -> URL {
+	func getUrlForFixed() -> URL {
 		let uuid: String = Auth.auth().currentUser?.uid ?? ""
 		return URL(string: "https://api.unusualcalendar.net/v2/missing/\(uuid)/fixed")!
+	}
+
+	func getUrlForFloating() -> URL {
+		let uuid: String = Auth.auth().currentUser?.uid ?? ""
+		return URL(string: "https://api.unusualcalendar.net/v2/missing/\(uuid)/floating")!
 	}
 
 	func getColor(reportState: ReportState) -> Color {
