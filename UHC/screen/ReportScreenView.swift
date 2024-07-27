75e96402f812a69a73a8b2aa6bd4031f1829214d
andret2344 <andret2344@gmail.com>
2024-07-27 08:24:23 +0200

    #2: Added about app button

--- UHC/screen/ReportScreenView.swift
+++ UHC/screen/ReportScreenView.swift
@@ -6,27 +6,42 @@ import SwiftUI
 import FirebaseAuth
 
 struct ReportScreenView: View {
+	@State private var showingAlert = false
+
 	var body: some View {
 		NavigationView {
 			List {
-				NavigationLink {
-					MissingHolidayScreenView()
-				} label: {
-					Label("Missing holiday?", systemImage: "pencil")
+				Section(header: Text("Reports")) {
+					NavigationLink {
+						MissingHolidayScreenView()
+					} label: {
+						Label("Missing holiday?", systemImage: "pencil")
+					}
+					NavigationLink {
+						SuggestionsScreenView()
+					} label: {
+						Label("My suggestions", systemImage: "checkmark")
+					}
+					NavigationLink {
+						MissingHolidayScreenView()
+					} label: {
+						Label("My reports", systemImage: "calendar.badge.exclamationmark")
+					}
+					.disabled(true)
 				}
-				NavigationLink {
-					SuggestionsScreenView()
-				} label: {
-					Label("My suggestions", systemImage: "checkmark")
+				.disabled(Auth.auth().currentUser?.isAnonymous ?? true)
+				Section(header: Text("Application")) {
+					Label("About calendar", systemImage: "info")
+						.contentShape(Rectangle())
+						.onTapGesture {
+							showingAlert = true
+						}
+						.alert("About calendar", isPresented: $showingAlert, actions: {}, message: {
+							Image("SadIcon")
+							Text("All holidays - even those most unusual - are an opportunity to look in a different way at people, and appreciate phenomena we usually ignore.")
+						})
 				}
-				NavigationLink {
-					MissingHolidayScreenView()
-				} label: {
-					Label("My reports", systemImage: "calendar.badge.exclamationmark")
-				}
-				.disabled(true)
 			}
-			.disabled(Auth.auth().currentUser?.isAnonymous ?? true)
 			.navigationTitle("Reports")
 		}
 		.scrollDisabled(true)
